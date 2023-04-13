1472bd7e7afec1695c737c5eefc2770c5ccb1d74
NusRAT-Lia <nusratlia424@gmail.com>
2023-04-13 20:41:43 +0600

    Tx updated

--- src/Transaction.h
+++ src/Transaction.h
@@ -1,52 +1,42 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h> // include all standard C++ headers
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
-#include "Transactions.h"
-#include "../Sha256_Algorithm/Sha256.h"
-using namespace std ;
+
+#include "Transactions.h" // include header file for Transactions class
+#include "../Sha256_Algorithm/Sha256.h" // include header file for SHA256 hash function
+
+using namespace std;
 
 class Transaction
-{       
-        private :
-            bool isVerified=false;
+{
+    private:
+        bool isVerified = false; // flag indicating if transaction has been verified
 
-            
+    public:
+        string TxHash; // hash of the transaction
+        pair<long long int,long long int> PublicKeyOfSenderWallet; // public key of the sender's wallet
+        pair<long long int,long long int> PublicKeyOfRecieverWallet; // public key of the receiver's wallet
+        long long int AmountSent; // amount of currency being sent
+        long long int Signature; // digital signature of the transaction
 
-        public :
-            string TxHash;
-            pair<long long int,long long int> PublicKeyOfSenderWallet;
-            pair<long long int,long long int> PublicKeyOfRecieverWallet;
-            long long int AmountSent;
-            //int TransactionNonce;
-            long long int Signature;
-            // Transactions TxList;
+        // constructor for Transaction class
+        Transaction(pair<long long int,long long int> SenderKey, pair<long long int,long long int> RecieverKey, int Amount, long long int Sign)
+        {
+            this->PublicKeyOfSenderWallet = SenderKey;
+            this->PublicKeyOfRecieverWallet = RecieverKey;
+            this->AmountSent = Amount;
+            this->Signature = Sign;
 
-            
-            
-            
-            
-            Transaction(pair<long long int,long long int> SenderKey , pair<long long int,long long int> RecieverKey , int Amount, long long int Sign )
-            {
-               this->PublicKeyOfSenderWallet=SenderKey;
-               this->PublicKeyOfRecieverWallet=RecieverKey;
-               this->AmountSent=Amount;
-               this->Signature=Sign;
+            // generate hash of transaction string
+            string TxString = to_string(this->PublicKeyOfSenderWallet.first) + to_string(this->PublicKeyOfRecieverWallet.first) + to_string(this->AmountSent) + to_string(this->Signature); 
+            this->TxHash = Hash(TxString);
+        }
 
-            //    srand(time(NULL));
-            //    this->TransactionNonce=rand()%1000 + 1; 
-               
-               string TxString=to_string(this->PublicKeyOfSenderWallet.first)+to_string(this->PublicKeyOfRecieverWallet.first)+to_string(this->AmountSent)+to_string(this->Signature); 
-               this->TxHash=Hash(TxString);
-
-            }
-
-            // void SetStatus(bool status)
-            // {
-            //     this->isVerified=status;
-            // }
-
-
-
+        // setter function for isVerified flag
+        // void SetStatus(bool status)
+        // {
+        //     this->isVerified = status;
+        // }
 };
 
 #endif
